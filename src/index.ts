268783155b051ac61117cc167de838a32d30e870
Paul Makles <paulmakles@gmail.com>
2020-08-12 21:51:07 +0200

    Add version information to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 export * from './Client';
 export * from './objects';
+
+export const API_VERSION = {
+    str: '0.2.8',
+    major: 0,
+    minor: 2,
+    patch: 8
+};
+
+export const LIBRARY_VERSION = {
+    str: '2.2.5',
+    major: 2,
+    minor: 2,
+    patch: 5
+}
