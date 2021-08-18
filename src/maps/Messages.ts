0a701f87095b2663af7dbb06187858993c8fd4e7
Kamil G <xmuffionex.dev@ogarnij.se>
2021-08-18 13:40:24 +0200

    async remove

--- src/maps/Messages.ts
+++ src/maps/Messages.ts
@@ -131,7 +131,7 @@ export class Message {
      * Reply to Message
      */
 
-    async reply(data: string | (Omit<Route<'POST', '/channels/id/messages'>["data"], 'nonce'> & { nonce?: string }), mention = true) {
+    reply(data: string | (Omit<Route<'POST', '/channels/id/messages'>["data"], 'nonce'> & { nonce?: string }), mention = true) {
         let obj = typeof data === 'string' ? { content: data } : data;
         return this.channel?.sendMessage({ ...obj, replies: [{ id: this._id, mention }] })
     }
