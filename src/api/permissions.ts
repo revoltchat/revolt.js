f5c5271d7bb57164663610fcc44a7a74f34fe9b6
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2021-08-20 10:18:40 +0200

    Left shift operator

--- src/api/permissions.ts
+++ src/api/permissions.ts
@@ -1,32 +1,32 @@
 export const UserPermission = {
-    Access: 0b00000000000000000000000000000001,      // 1
-    ViewProfile: 0b00000000000000000000000000000010, // 2
-    SendMessage: 0b00000000000000000000000000000100, // 4
-    Invite: 0b00000000000000000000000000001000,      // 8
+    Access: 1 << 0,
+    ViewProfile: 1 << 1,
+    SendMessage: 1 << 2,
+    Invite: 1 << 3,
 }
 
 export const ChannelPermission = {
-    View: 0b00000000000000000000000000000001,           // 1
-    SendMessage: 0b00000000000000000000000000000010,    // 2
-    ManageMessages: 0b00000000000000000000000000000100, // 4
-    ManageChannel: 0b00000000000000000000000000001000,  // 8
-    VoiceCall: 0b00000000000000000000000000010000,      // 16
-    InviteOthers: 0b00000000000000000000000000100000,   // 32
-    EmbedLinks: 0b00000000000000000000000001000000,   // 64
-    UploadFiles: 0b00000000000000000000000010000000,   // 128
+    View: 1 << 0,
+    SendMessage: 1 << 1,
+    ManageMessages: 1 << 2,
+    ManageChannel: 1 << 3,
+    VoiceCall: 1 << 4,
+    InviteOthers: 1 << 5,
+    EmbedLinks: 1 << 6,
+    UploadFiles: 1 << 7,
 }
 
 export const ServerPermission = {
-    View: 0b00000000000000000000000000000001,            // 1
-    ManageRoles: 0b00000000000000000000000000000010,   // 2
-    ManageChannels: 0b00000000000000000000000000000100,  // 4
-    ManageServer: 0b00000000000000000000000000001000,    // 8
-    KickMembers: 0b00000000000000000000000000010000,     // 16
-    BanMembers: 0b00000000000000000000000000100000,      // 32
-    ChangeNickname: 0b00000000000000000001000000000000,  // 4096
-    ManageNicknames: 0b00000000000000000010000000000000, // 8192
-    ChangeAvatar: 0b00000000000000000100000000000000,    // 16382
-    RemoveAvatars: 0b00000000000000001000000000000000,   // 32768
+    View: 1 << 0,
+    ManageRoles: 1 << 1,
+    ManageChannels: 1 << 2,
+    ManageServer: 1 << 3,
+    KickMembers: 1 << 4,
+    BanMembers: 1 << 5,
+    ChangeNickname: 1 << 12,
+    ManageNicknames: 1 << 13,
+    ChangeAvatar: 1 << 14,
+    RemoveAvatars: 1 << 15,
 }
 
 export const U32_MAX = 2**32 - 1; // 4294967295
