047b66b04342d82323b9a54a0e1edb8aad0db58c
Paul <paulmakles@gmail.com>
2021-12-11 18:20:43 +0000

    fix: mark user as observable

--- src/Client.ts
+++ src/Client.ts
@@ -120,6 +120,7 @@ export class Client extends EventEmitter {
         makeObservable(
             this,
             {
+                user: observable,
                 users: observable,
                 channels: observable,
                 servers: observable,
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const LIBRARY_VERSION = "5.1.0-alpha.11";
+export const LIBRARY_VERSION = "5.1.0-alpha.12";
 
 export const defaultConfig = {
     apiURL: "https://api.revolt.chat",
